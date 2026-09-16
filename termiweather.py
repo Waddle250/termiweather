7dc8f949c9435f8bc7bccbb2243ffdeb86960e55
Waddle250 <loganaquino11@hotmail.com>
2026-09-16 13:40:23 -0400

    added an error

--- termiweather.py
+++ termiweather.py
@@ -4,11 +4,14 @@ import asyncio
 import sys
 
 async def getWeather(location) -> None:
-    weather = await client.get(str(location))
+    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+        weather = await client.get(str(location))
 
-    print("Temperature: " + weather.temperature)
-    print("Precipitation: " + weather.precipitation)
-    print("Wind Speed and Direction: " + weather.wind speed + weather.wind_direction)
-    print("Humidity: " + weather.humidity)
+        print("Temperature: " + weather.temperature)
+        print("Precipitation: " + weather.precipitation)
+        print("Wind Speed and Direction: " + weather.wind speed + weather.wind_direction)
+        print("Humidity: " + weather.humidity)
 
-getWeather(sys.argv[0])
+if __name__ == '__main__':
+    location = sys.argv[1]
+    asyncio.run(getWeather(location))
