4325c764c06da4eca85a78a58dad5009bd3edb51
Waddle250 <loganaquino11@hotmail.com>
2026-09-16 13:14:54 -0400

    Update termiweather.py

--- termiweather.py
+++ termiweather.py
@@ -1,9 +1,12 @@
 import python_weather
 import asyncio
+import sys
 
-async def main(location) -> None:
+async def getWeather(location) -> None:
     weather = await client.get(str(location))
 
     print(weather.temperature)
     print(weather.precipitation)
     print(weather.wind speed + weather.wind_direction)
+
+getWeather(sys.argv[0])
