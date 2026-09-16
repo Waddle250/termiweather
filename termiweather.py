182660686e94a71bd4fdb5acbfc8505638e724fe
Waddle250 <loganaquino11@hotmail.com>
2026-09-16 11:51:04 -0400

    added an error

--- termiweather.py
+++ termiweather.py
@@ -1,1 +1,9 @@
-import python-weather
+import python_weather
+import asyncio
+
+async def main(location) -> None:
+    weather = await client.get(str(location))
+
+    print(weather.temperature)
+    print(weather.precipitation)
+    print(weather.wind speed + weather.wind_direction)
